90a81d8496ae4b1db5e344f208c4dfd7de192e96
Serhii Ilnytskyi <serhiiilnytskyi@gmal.com>
2019-05-14 19:02:59 +0300

    Thread Executor

--- src/lesson2/ThreadPoolDemoMain.java
+++ src/lesson2/ThreadPoolDemoMain.java
@@ -1,4 +1,20 @@
 package lesson2;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolDemoMain {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            System.out.println("hello");
+        });
+        executorService.submit(() -> {
+            System.out.println("hello2");
+        });
+        executorService.submit(() -> {
+            System.out.println("hello3");
+        });
+        executorService.shutdown();
+    }
 }
