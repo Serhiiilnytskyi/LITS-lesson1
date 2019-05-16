46d21841d3a8f08d8ab392dd2364e7ffb63a1a33
Serhii Ilnytskyi <serhiiilnytskyi@gmal.com>
2019-05-16 18:00:56 +0300

    Complete HT to lesson2 with rannuble

--- src/lesson2/HomeTask/Client.java
+++ src/lesson2/HomeTask/Client.java
@@ -4,14 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
-public class Client implements Callable<String> {
+public class Client implements Runnable {
+    private String word;
+    private boolean isNewWord = false;
+    private static boolean waitForInput = true;
 
     @Override
-    public String call() throws IOException {
+    public void run() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String word = null;
-        System.out.print("Enter word: ");
-        return reader.readLine();
+        String word = "";
+
+        while (!word.equals("stop")) {
+            if (!isNewWord && waitForInput) {
+                try {
+                    waitForInput = false;
+                    System.out.print("Enter new word: ");
+
+                    this.word = reader.readLine();
+                    isNewWord = true;
+                    waitForInput = true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    synchronized (this) {
+                        this.wait(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public boolean isNewWord() {
+        return isNewWord;
+    }
+
+    public void setNewWord(boolean newWord) {
+        isNewWord = newWord;
     }
 }
--- src/lesson2/HomeTask/Server.java
+++ src/lesson2/HomeTask/Server.java
@@ -1,33 +1,45 @@
 package lesson2.HomeTask;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class Server {
-    List<Callable<String>> clients;
-    ExecutorService excutor = Executors.newFixedThreadPool(2);
+    List<Client> clients;
+    ExecutorService executor = Executors.newFixedThreadPool(2);
     List<Future<String>> futures = new ArrayList<Future<String>>();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        List<Callable<String>> clients= new ArrayList<>();
+        List<Client> clients = new ArrayList<>();
         Client client1 = new Client();
+        Client client2 = new Client();
         clients.add(client1);
+        clients.add(client2);
         Server server = new Server(clients);
     }
 
-    public Server(List<Callable<String>> clients) throws ExecutionException, InterruptedException {
+    public Server(List<Client> clients) throws ExecutionException, InterruptedException {
         this.clients = clients;
+        String word = "";
         for (int i = 0; i < clients.size(); i++) {
-            Future<String> future = excutor.submit(clients.get(i));
-            futures.add(future);
-            System.out.println(isPolydrom(future.get()));
+            executor.execute(clients.get(i));
         }
+        while (true) {
+            for (int i = 0; i <clients.size(); i++) {
+                if (clients.get(i).isNewWord()) {
+                    System.out.println("Client " + (i + 1) + " send this word: " +
+                            clients.get(i).getWord() + "-" +
+                            (isPolydrom(clients.get(i).getWord()) ? "this is polindome" : "this isn't polindrome"));
 
+                    clients.get(i).setNewWord(false);
+                }
+            }
+        }
     }
 
-    public Server(Callable client) {
+
+
+    public Server(Runnable client) {
 /*        this.clients = new List<Callable>() {
         }*/
     }
